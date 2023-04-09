8d0967d77bd34e2cf5cf295097ee3ba9655e7f40
Hardik Bajaj <bajajhardik0706@gmail.com>
2023-04-10 01:55:43 +0530

    improved commit

--- ConnectivityStatus/src/main/java/com/internetconnectivity/connectivitystatus/ConnectivityStatus.kt
+++ ConnectivityStatus/src/main/java/com/internetconnectivity/connectivitystatus/ConnectivityStatus.kt
@@ -1,6 +1,8 @@
 package com.internetconnectivity.connectivitystatus
 
 import android.content.Context
+import android.widget.Toast
+import androidx.lifecycle.Lifecycle
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -9,50 +11,63 @@ class ConnectivityStatus(context: Context) {
 
     private val connectivityStatus:ConnectivityMonitor = CheckConnectivity(context)
 
+    private val scope = CoroutineScope(Dispatchers.Main)
 
-    fun available(onAvailable:()->Unit){
+//    inline fun monitorConnectivityStatus(
+//        crossinline onAvailable:()->Unit={ },
+//        crossinline onLost: () -> Unit={ },
+//        crossinline unAvailable: () -> Unit={ },
+//        crossinline losing: () -> Unit={ }
+//    )=CoroutineScope(Dispatchers.Main).launch {
+//        connectivityStatus.observer().collect{
+//            when(it) {
+//                ConnectivityMonitor.Status.Available -> onAvailable()
+//                ConnectivityMonitor.Status.UnAvailable -> unAvailable()
+//                ConnectivityMonitor.Status.Losing -> losing()
+//                ConnectivityMonitor.Status.Lost -> onLost()
+//            }
+//        }
+//    }
 
-        CoroutineScope(Dispatchers.Main).launch {
+
+    fun available(onAvailable:()->Unit) = scope.launch{
+
             connectivityStatus.observer().collect{
                 if(it == ConnectivityMonitor.Status.Available){
                     onAvailable()
                 }
             }
-        }
+
 
     }
 
-    fun unAvailable(unAvailable:()->Unit){
-        CoroutineScope(Dispatchers.Main).launch {
+    fun unAvailable(unAvailable:()->Unit) = scope.launch{
             connectivityStatus.observer().collect{
                 if(it == ConnectivityMonitor.Status.UnAvailable){
                     unAvailable()
                 }
             }
 
-        }
     }
 
-    fun losing(losing:()->Unit){
-        CoroutineScope(Dispatchers.Main).launch {
+    fun losing(losing:()->Unit) = scope.launch{
             connectivityStatus.observer().collect{
                 if(it == ConnectivityMonitor.Status.Losing){
                     losing()
                 }
             }
 
-        }
     }
 
-    fun onLost(onLost:()->Unit){
-        CoroutineScope(Dispatchers.Main).launch {
+    fun onLost(onLost:()->Unit)=scope.launch{
             connectivityStatus.observer().collect{
                 if(it == ConnectivityMonitor.Status.Lost){
                     onLost()
                 }
             }
 
-        }
     }
+
+
     
 }
--- app/src/main/java/com/internetConnectivity/monitorinternetconnectivity/MainActivity.kt
+++ app/src/main/java/com/internetConnectivity/monitorinternetconnectivity/MainActivity.kt
@@ -4,23 +4,41 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.internetconnectivity.connectivitystatus.ConnectivityStatus
+import kotlinx.coroutines.CoroutineScope
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+//        connectivityStatus.available {
+//            Toast.makeText(this@MainActivity,"Adfadf",Toast.LENGTH_LONG).show()
+//        }
+//
+//        connectivityStatus.onLost {
+//            Toast.makeText(this@MainActivity,"Internet Connection Lost",Toast.LENGTH_LONG).show()
+//        }
+
+
+//        connectivityStatus.monitorConnectivityStatus(onAvailable = {
+//            Toast.makeText(this@MainActivity,"Adfadf",Toast.LENGTH_LONG).show()
+//        },
+//        onLost = {
+//            Toast.makeText(this@MainActivity,"Adfadfdsfdfsgf",Toast.LENGTH_LONG).show()
+//        })
+
+    }
+
+    override fun onResume() {
+        super.onResume()
         val connectivityStatus = ConnectivityStatus(this)
         connectivityStatus.available {
             Toast.makeText(this@MainActivity,"Adfadf",Toast.LENGTH_LONG).show()
         }
 
         connectivityStatus.onLost {
-            Toast.makeText(this@MainActivity,"Internet Connection Lost",Toast.LENGTH_LONG).show()
+            Toast.makeText(this@MainActivity,"Adfgsdgsafgadf",Toast.LENGTH_LONG).show()
         }
-
-
-
-
     }
 }
